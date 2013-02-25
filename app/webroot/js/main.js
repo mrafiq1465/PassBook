1d00bf47bdcdba2831141811066a4ed77b141dd4
raf-ampush <raf.islam@ampushmedia.com>
2013-02-25 13:16:34 +0600

    fixes

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
                             return;
                         }
                         var tabNumber = parseInt($form.attr('id').substr(4,1));
-                        if(tabNumber !== 6) {
+                        if(tabNumber !== 5) {
                             tabNumber++;
                             var $tabToActivate = $('#tab' + tabNumber);
                             $tabStrip.data('kendoTabStrip').activateTab($tabToActivate);
