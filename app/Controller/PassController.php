86a89c48af095fcb8c8eb209ff9f34e152645aea
raf-ampush <raf.islam@ampush.com>
2013-05-03 01:35:41 +0600

    Add image resize class

--- app/Controller/PassController.php
+++ app/Controller/PassController.php
@@ -115,6 +115,24 @@ class PassController extends AppController
                         $this->Pass->data['Pass']['iconImage'] = str_replace(WWW_ROOT, '', $destination_path_icon);
                         $this->Pass->data['Pass']['logoImage'] = str_replace(WWW_ROOT, '', $destination_path_logo);
 
+                        // Image resize
+                        $image = new SimpleImage();
+                        $image->load($destination_path_logo);
+
+                        // for logo
+                        $image->resizeToWidth(150);
+                        $image->save($destination_path_logo);
+
+                        // for icon
+                        $image->resizeToWidth(100);
+                        $image->save($destination_path_icon);
+
+                        // for icon 2x
+                       #$image->resizeToWidth(200);
+                       #$image->save($destination_path_logo);
+
+                        // for other goes here
+
                     }
                     if($file_name == "strip@2x") {
                         $destination_path_strip = $destination_dir . "strip.png";
@@ -381,4 +399,87 @@ class PassController extends AppController
         $this->ajax_response(array('success' => true));
 
     }
+}
+
+class SimpleImage {
+
+   var $image;
+   var $image_type;
+
+   function load($filename) {
+
+      $image_info = getimagesize($filename);
+      $this->image_type = $image_info[2];
+      if( $this->image_type == IMAGETYPE_JPEG ) {
+
+         $this->image = imagecreatefromjpeg($filename);
+      } elseif( $this->image_type == IMAGETYPE_GIF ) {
+
+         $this->image = imagecreatefromgif($filename);
+      } elseif( $this->image_type == IMAGETYPE_PNG ) {
+
+         $this->image = imagecreatefrompng($filename);
+      }
+   }
+   function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $permissions=null) {
+
+      if( $image_type == IMAGETYPE_JPEG ) {
+         imagejpeg($this->image,$filename,$compression);
+      } elseif( $image_type == IMAGETYPE_GIF ) {
+
+         imagegif($this->image,$filename);
+      } elseif( $image_type == IMAGETYPE_PNG ) {
+
+         imagepng($this->image,$filename);
+      }
+      if( $permissions != null) {
+
+         chmod($filename,$permissions);
+      }
+   }
+   function output($image_type=IMAGETYPE_JPEG) {
+
+      if( $image_type == IMAGETYPE_JPEG ) {
+         imagejpeg($this->image);
+      } elseif( $image_type == IMAGETYPE_GIF ) {
+
+         imagegif($this->image);
+      } elseif( $image_type == IMAGETYPE_PNG ) {
+
+         imagepng($this->image);
+      }
+   }
+   function getWidth() {
+
+      return imagesx($this->image);
+   }
+   function getHeight() {
+
+      return imagesy($this->image);
+   }
+   function resizeToHeight($height) {
+
+      $ratio = $height / $this->getHeight();
+      $width = $this->getWidth() * $ratio;
+      $this->resize($width,$height);
+   }
+
+   function resizeToWidth($width) {
+      $ratio = $width / $this->getWidth();
+      $height = $this->getheight() * $ratio;
+      $this->resize($width,$height);
+   }
+
+   function scale($scale) {
+      $width = $this->getWidth() * $scale/100;
+      $height = $this->getheight() * $scale/100;
+      $this->resize($width,$height);
+   }
+
+   function resize($width,$height) {
+      $new_image = imagecreatetruecolor($width, $height);
+      imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+      $this->image = $new_image;
+   }
+
 }
